2fcd1b37a10670cc44f7b79740a6da181a81694b
ShinHB417 <heebum417@naver.com>
2023-05-25 16:08:21 +0900

    feat:초기파일 업로드

---
+++ src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
@@ -0,0 +1,14 @@
+package com.konkuk.Eodikase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class EodikaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodikaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/Eodikase/EodikaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.Eodikase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodikaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
