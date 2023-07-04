11fad7e575c638571d4b8e9a29d6002415fa6255
heebeom <heebum417@gmail.com>
2023-07-04 12:43:07 +0900

    feat : 테스트 컨트롤러 작성

---
+++ src/main/java/com/konkuk/Eodikase/TestController.java
@@ -0,0 +1,14 @@
+package com.konkuk.Eodikase;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "어디카세 테스트 페이지";
+    }
+}
