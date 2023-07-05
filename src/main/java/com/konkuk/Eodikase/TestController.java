8a400dffc8e567015c0d5acbdc6381ab502d6dbf
heebeom <heebum417@gmail.com>
2023-07-05 16:34:22 +0900

    feat : jenkins3 테스트 컨트롤러 작성

--- src/main/java/com/konkuk/Eodikase/TestController.java
+++ src/main/java/com/konkuk/Eodikase/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test(){
-        return "jenkins1  build 어디카세 테스트 페이지";
+        return "jenkins3  build 어디카세 테스트 페이지";
     }
 }
