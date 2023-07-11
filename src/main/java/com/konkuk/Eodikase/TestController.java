6664e52e0301f0783dcbf228ece7024d192682ad
heebeom <heebum417@gmail.com>
2023-07-11 12:54:00 +0900

    jenkins test

--- src/main/java/com/konkuk/Eodikase/TestController.java
+++ src/main/java/com/konkuk/Eodikase/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test(){
-        return "jenkins9  build 어디카세 테스트 페이지";
+        return "jenkins10  build 어디카세 테스트 페이지";
     }
 }
