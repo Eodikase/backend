ab08ec59bc90e70bfef62ef99754401678a2b3aa
dlawltn0708 <dlawotn3@naver.com>
2023-07-18 00:26:23 +0900

    feat: 이메일 회원가입 dto 추가

---
+++ src/main/java/com/konkuk/Eodikase/dto/request/MemberSignUpRequest.java
@@ -0,0 +1,16 @@
+package com.konkuk.Eodikase.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class MemberSignUpRequest {
+
+    private String email;
+    private String password;
+    private String nickname;
+}
