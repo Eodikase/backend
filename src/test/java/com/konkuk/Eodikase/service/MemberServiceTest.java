092c141033e71759e566faf0413ced4e8bf667c2
dlawltn0708 <dlawotn3@naver.com>
2023-07-18 16:01:23 +0900

    test: 회원가입 관련 테스트 코드 추가

--- src/test/java/com/konkuk/Eodikase/service/MemberServiceTest.java
+++ src/test/java/com/konkuk/Eodikase/service/MemberServiceTest.java
@@ -2,16 +2,25 @@ package com.konkuk.Eodikase.service;
 
 import com.konkuk.Eodikase.domain.member.dto.request.MemberSignUpRequest;
 import com.konkuk.Eodikase.domain.member.entity.Member;
+import com.konkuk.Eodikase.domain.member.entity.MemberPlatform;
 import com.konkuk.Eodikase.domain.member.repository.MemberRepository;
 import com.konkuk.Eodikase.domain.member.service.MemberService;
+import com.konkuk.Eodikase.exception.badrequest.DuplicateMemberException;
+import com.konkuk.Eodikase.exception.badrequest.DuplicateNicknameException;
+import com.konkuk.Eodikase.exception.badrequest.InvalidPasswordException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class MemberServiceTest {
@@ -20,17 +29,77 @@ public class MemberServiceTest {
     private MemberRepository memberRepository;
     @Autowired
     private MemberService memberService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("회원을 정상적으로 가입한다")
     void signUp() {
         String expected = "dlawotn3@naver.com";
-        MemberSignUpRequest request = new MemberSignUpRequest(expected, "a1b2c3d4!", "감자");
+        MemberSignUpRequest request = new MemberSignUpRequest(expected, "edks1234!", "감자");
 
         memberService.signUp(request);
 
         List<Member> actual = memberRepository.findAll();
         assertThat(actual).hasSize(1);
         assertThat(actual.get(0).getEmail()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("이미 가입된 이메일이 존재하면 회원 가입 시에 예외를 반환한다")
+    void signUpByDuplicateEmailMember() {
+        String email = "dlawotn3@naver.com";
+        memberRepository.save(new Member(email, "edks1234!", "감자", MemberPlatform.HOME));
+        MemberSignUpRequest request = new MemberSignUpRequest(email, "edks1234!", "어디카세");
+
+        assertThatThrownBy(() -> memberService.signUp(request))
+                .isInstanceOf(DuplicateMemberException.class);
+    }
+
+    @Test
+    @DisplayName("이미 가입된 닉네임이 존재하면 회원 가입 시에 예외를 반환한다")
+    void signUpByDuplicateNicknameMember() {
+        String nickname = "감자";
+        memberRepository.save(new Member("dlawotn3@naver.com", "edks1234!", nickname,
+                MemberPlatform.HOME));
+        MemberSignUpRequest request = new MemberSignUpRequest("eodikase3@naver.com", "edks1234!",
+                nickname);
+
+        assertThatThrownBy(() -> memberService.signUp(request))
+                .isInstanceOf(DuplicateNicknameException.class);
+    }
+
+    @Test
+    @DisplayName("회원 비밀번호를 정상적으로 암호화한다")
+    void encryptPassword() {
+        String rawPassword = "1234";
+
+        String encryptedPassword = passwordEncoder.encode(rawPassword);
+
+        Boolean matches = passwordEncoder.matches(rawPassword, encryptedPassword);
+        assertThat(matches).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("비밀번호가 8~15자가 아니면 예외를 반환한다")
+    @ValueSource(strings = {"abcdef7", "abcdefgabcdefgabc21"})
+    void passwordLengthValidation(String password) {
+        assertThatThrownBy(() -> memberService.signUp(new MemberSignUpRequest("dlawotn3@naver.com", password,
+                "감자")))
+                .isInstanceOf(InvalidPasswordException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("비밀번호가 영어, 숫자, 특수문자를 모두 포함하지 않으면 예외를 반환한다")
+    @ValueSource(strings = {"abcdefgh", "12345678", "abc1234", "abc!!!!"})
+    void passwordConfigureValidation(String password) {
+        assertThatThrownBy(() -> memberService.signUp(new MemberSignUpRequest("dlawotn3@naver.com",
+                password, "감자")))
+                .isInstanceOf(InvalidPasswordException.class);
     }
 }
