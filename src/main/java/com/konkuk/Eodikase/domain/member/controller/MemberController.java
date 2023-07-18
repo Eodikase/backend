9d6960940cff4c8a5ba95aa8ec3df378232d9569
dlawltn0708 <dlawotn3@naver.com>
2023-07-18 16:40:48 +0900

    feat: swagger 설정 추가

--- src/main/java/com/konkuk/Eodikase/domain/member/controller/MemberController.java
+++ src/main/java/com/konkuk/Eodikase/domain/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.konkuk.Eodikase.domain.member.controller;
 import com.konkuk.Eodikase.domain.member.dto.request.MemberSignUpRequest;
 import com.konkuk.Eodikase.domain.member.dto.response.MemberSignUpResponse;
 import com.konkuk.Eodikase.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "Members", description = "회원")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/members")
@@ -19,6 +22,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입")
     @PostMapping
     public ResponseEntity<MemberSignUpResponse> signUp(@RequestBody @Valid MemberSignUpRequest request) {
         MemberSignUpResponse response = memberService.signUp(request);
