086e37d8a725bf2af34d4256484a26d4b2a3f160
heebeom <heebum417@gmail.com>
2023-08-10 16:41:21 +0900

    feat : courseData 지역별 테이블 생성

---
+++ src/main/java/com/konkuk/Eodikase/domain/audit/BaseCourseEntity.java
@@ -0,0 +1,31 @@
+package com.konkuk.Eodikase.domain.audit;
+
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseCourseEntity {
+    private String name;
+
+    private String category;
+
+    private String location;
+
+    private String phoneNumber;
+
+    private Float scoreByNaver;
+
+    private String href;
+
+    private String operatingTime;
+
+    private int reviewCount;
+
+    private String imageUrl;
+
+}
---
+++ src/main/java/com/konkuk/Eodikase/domain/data/entity/CourseDataHI.java
@@ -0,0 +1,25 @@
+package com.konkuk.Eodikase.domain.data.entity;
+
+import com.konkuk.Eodikase.domain.audit.BaseCourseEntity;
+import com.konkuk.Eodikase.domain.course.entity.CourseCourseDataRel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Getter
+@Setter
+public class CourseDataHI extends BaseCourseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="course_data_hi_id")
+    private Long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private com.konkuk.Eodikase.domain.data.entity.CourseDataCategory CourseDataCategory;
+
+    @OneToMany(mappedBy = "courseDataHI")
+    private List<CourseCourseDataRel> courseDataList = new ArrayList<>();
+}
