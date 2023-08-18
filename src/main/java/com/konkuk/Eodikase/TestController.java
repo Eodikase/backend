a7ddddfa7184c1a0d7f04d2654b10bbd7e94286c
HeeBeom <92363927+ShinHB417@users.noreply.github.com>
2023-08-18 14:50:09 +0900

    Update TestController.java

--- src/main/java/com/konkuk/Eodikase/TestController.java
+++ src/main/java/com/konkuk/Eodikase/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test(){
-        return "githubactionci/cd test in running plz!!!!";
+        return "githubactionci/cd test in running plzplz!!!!";
     }
 }
