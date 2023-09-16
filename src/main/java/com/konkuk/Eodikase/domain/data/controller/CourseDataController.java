4b0abb88d5758488ec06ea7232adf24a4aa98b71
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-09-16 23:18:02 +0900

    feat: 타입별 코스 아이템 조회 컨트롤러 메서드 추가

--- src/main/java/com/konkuk/Eodikase/domain/data/controller/CourseDataController.java
+++ src/main/java/com/konkuk/Eodikase/domain/data/controller/CourseDataController.java
@@ -3,11 +3,25 @@ package com.konkuk.Eodikase.domain.data.controller;
 import com.konkuk.Eodikase.domain.data.service.CourseDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/courseDatas")
 public class CourseDataController {
 
     private final CourseDataService courseDataService;
 
+    @Operation(summary = "타입별 코스 아이템 조회")
+    @GetMapping
+    public ResponseEntity<GetFilteredCourseDataResponse> getFilteredCourseData(
+            @RequestParam String region, @RequestParam String types
+    ) {
+        GetFilteredCourseDataResponse response = courseDataService.getFilteredCourseData(region, types);
+        return ResponseEntity.ok(response);
+    }
 }
