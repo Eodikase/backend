13b0c9a314b4bdd7e6bf6fee8b8de3cb0159b31c
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-09-23 04:14:51 +0900

    delete: 사용하지 않는 예외 파일 삭제

--- src/main/java/com/konkuk/Eodikase/exception/badrequest/InvalidOrderException.java
+++
@@ -1,8 +0,0 @@
-package com.konkuk.Eodikase.exception.badrequest;
-
-public class InvalidOrderException extends BadRequestException {
-
-    public InvalidOrderException() {
-        super("순서가 올바르지 않습니다.", 3002);
-    }
-}
