36ae74b714e786d3a70a4097bbc54a55aa3f34a9
ShinHB417 <heebum417@naver.com>
2023-09-25 23:40:02 +0900

    feat : 해시태그관련 api 개발

---
+++ src/main/java/com/konkuk/Eodikase/domain/hashtag/repository/HashtagRepository.java
@@ -0,0 +1,8 @@
+package com.konkuk.Eodikase.domain.hashtag.repository;
+
+import com.konkuk.Eodikase.domain.hashtag.entity.Hashtag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashtagRepository extends JpaRepository<Hashtag,Long> {
+
+}
