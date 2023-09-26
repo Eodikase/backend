639d54dcbf5e3831faa013d9905f7c4a65d12a62
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-09-26 16:14:23 +0900

    fix: response 바디 필드명 변경

--- src/main/java/com/konkuk/Eodikase/dto/response/data/FilteredCourseDataResponse.java
+++ src/main/java/com/konkuk/Eodikase/dto/response/data/FilteredCourseDataResponse.java
@@ -9,6 +9,6 @@ import java.util.List;
 @AllArgsConstructor
 public class FilteredCourseDataResponse {
     private String datasType;
-    private List<FilteredCourseDataByRadiusResponse> contents;
+    private List<FilteredCourseDataByRadiusResponse> content;
     private Boolean last;
 }
