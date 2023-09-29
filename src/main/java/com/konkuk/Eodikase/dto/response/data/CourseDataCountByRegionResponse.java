1daaa5a65ecdfbae51a78f4c287981b532f5e6f3
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-09-29 15:20:20 +0900

    feat: 지역별 코스 아이템 개수 조회 컨트롤러 메서드 생성

---
+++ src/main/java/com/konkuk/Eodikase/dto/response/data/CourseDataCountByRegionResponse.java
@@ -0,0 +1,10 @@
+package com.konkuk.Eodikase.dto.response.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CourseDataCountByRegionResponse {
+    private int count;
+}
