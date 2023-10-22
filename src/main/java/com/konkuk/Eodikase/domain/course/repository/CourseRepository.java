6df1ebd495ecf8cc0039560fefa86567104bd563
dlawotn3 <imjisu@imjisuui-MacBookPro.local>
2023-10-22 17:35:43 +0900

    feat: 스크랩 조회 관련 레포지토리 메서드 추가

--- src/main/java/com/konkuk/Eodikase/domain/course/repository/CourseRepository.java
+++ src/main/java/com/konkuk/Eodikase/domain/course/repository/CourseRepository.java
@@ -6,13 +6,16 @@ import com.konkuk.Eodikase.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course,Long> {
     Page<Course> findAllByIsOpen(Boolean open, Pageable pageable);
     Page<Course> findAllByRegionAndIsOpen(CourseRegion region,Boolean open, Pageable pageable);
     Page<Course> findAllByMemberAndIsOpen(Member member, Boolean open, Pageable pageable);
+    @Query("SELECT c FROM Course c WHERE c.id IN :courseIds")
+    Page<Course> findCoursesByIdIn(@Param("courseIds") List<Long> courseIds, Pageable pageable);
     void deleteByMember(Member member);
 }
