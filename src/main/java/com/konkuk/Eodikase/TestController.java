4c72770005a59afc2cbd4d5a7fda4f13b6b657a8
ShinHB417 <heebum417@naver.com>
2023-10-27 16:26:09 +0900

    fix : 자동 배포 테스트

--- src/main/java/com/konkuk/Eodikase/TestController.java
+++ src/main/java/com/konkuk/Eodikase/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         System.out.println(org.hibernate.Version.getVersionString());
-        return "yml 관련 서브모듈 브랜치 업데이트 ";
+        return "yml 관련 서브모듈 브랜치 업데이트2 ";
 
     }
 }
