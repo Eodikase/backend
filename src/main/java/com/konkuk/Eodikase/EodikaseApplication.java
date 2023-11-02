dbc2f6085426e51123b6cc491012c60d308878b9
Ji-soo708 <dlawotn3@naver.com>
2023-11-02 15:19:34 +0900

    feat: ci/cd

--- src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
+++ src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EodikaseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EodikaseApplication.class, args);
 
