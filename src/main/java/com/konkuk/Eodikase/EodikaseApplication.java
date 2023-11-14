c408c4a64ecef7a8fec2373c8e71770be2b213cd
Ji-soo708 <dlawotn3@naver.com>
2023-11-14 20:55:19 +0900

    style: 토큰 유효기간 변경

--- src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
+++ src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EodikaseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EodikaseApplication.class, args);
-
 	}
 }
