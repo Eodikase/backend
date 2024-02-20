60e9b02d30c138c3d083a6688cfb60441442bfd0
ShinHB417 <heebum417@naver.com>
2024-02-20 15:12:26 +0900

    fix : rds password 변경

--- src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
+++ src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EodikaseApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(EodikaseApplication.class, args);
 
 	}
