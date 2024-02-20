7e703258ac55d04ac872d6fcf9ec5c6ae038db18
ShinHB417 <heebum417@naver.com>
2024-02-20 15:17:53 +0900

    fix : 서브모듈 변경

--- src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
+++ src/main/java/com/konkuk/Eodikase/EodikaseApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EodikaseApplication {
 	public static void main(String[] args) {
-
 		SpringApplication.run(EodikaseApplication.class, args);
-
 	}
 }
